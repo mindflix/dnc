c0ad840e8b6bc2bbf3eb680b930cd34a0b30bdb6
mindflix <nyco.demol@gmail.com>
2021-10-15 12:56:07 +0200

    lea ma saoule

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import Hero from "components/Hero"
-import { Container, Wrap } from "styles/globalComponents"
-
-export default function Home() {
-  return (
-        <Hero></Hero>
-  )
-}
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Container, Button } from '@components/ui'
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <Button>Obtenir un RDV</Button>
+    </Container>
+  )
+}
+
+export default Home
